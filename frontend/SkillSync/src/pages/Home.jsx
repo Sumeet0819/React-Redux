19c27837dcf4544c04152e7d5caa6242e65fe4ce
Sumeet0819 <157307366+Sumeet0819@users.noreply.github.com>
2025-07-30 14:40:33 +0530

    first Commit

---
+++ frontend/SkillSync/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import axios from "../utils/axios"
+const Home = () => {
+
+   const getdata = async ()=>  {
+    try{
+      const {data} = await axios.get('/comments')
+      console.log(data);
+ 
+    }catch(error){
+        console.log(error);
+        
+    }
+   }
+
+   useEffect(() => {
+     getdata()
+   }, [])
+   
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
